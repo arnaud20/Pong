faf74361898b5a5dbd290564ba80bce9101fbe0e
arnaud20 <arnaud20@up.edu>
2018-03-21 18:47:06 -0700

    Part 1 complete!

--- app/src/main/java/com/example/alyarnaud/pong/Ball.java
+++ app/src/main/java/com/example/alyarnaud/pong/Ball.java
@@ -1,8 +1,31 @@
 package com.example.alyarnaud.pong;
 
+import java.util.Random;
+
 /**
  * Created by alyarnaud on 3/21/2018.
  */
 
 public class Ball {
+
+    public int numX; //x position of the ball
+    public int numY;//y position of the ball
+
+    public int xSpeed=15;
+    public int ySpeed=15;
+
+
+
+    public Ball(int xClick, int yClick ){
+        this.numX = xClick;
+        this.numY = yClick;
+    }
+    public void reverseXSpeed() {
+        // set our instance variable
+        xSpeed = -xSpeed;
+    }
+    public void reverseYSpeed(){
+        ySpeed = -ySpeed;
+
+    }
 }
--- app/src/main/java/com/example/alyarnaud/pong/TestAnimator.java
+++ app/src/main/java/com/example/alyarnaud/pong/TestAnimator.java
@@ -5,9 +5,9 @@ package com.example.alyarnaud.pong;
  */
 
         import android.graphics.*;
-        import android.util.Log;
         import android.view.MotionEvent;
 
+        import java.util.ArrayList;
         import java.util.Random;
 
 
@@ -22,24 +22,22 @@ package com.example.alyarnaud.pong;
 public class TestAnimator implements Animator {
 
     // instance variables
-    //private int xcount = 0; // counts the number of logical clock ticks
-    //private int ycount = 0; // counts the number of logical clock ticks
-
-    private int numX;
-    private int numY;
-
-    private boolean reverseX = false; // whether clock is ticking backwards
-    private boolean reverseY = false; // whether clock is ticking backwards
-
     private int BOX_WIDTH = 2560; //width of tablet
     private int BOX_HEIGHT = 1600; // height of tablet
 
-    private Random randomX = new Random();
-    private Random randomY = new Random();
+    private Random r = new Random();
 
+    //these will live in Ball
     private int xSpeed=15;
     private int ySpeed=15;
+    private int numX; //x position of the ball
+    private int numY;//y position of the ball
 
+    /**
+     * unsure of how to set up an arraylist
+     * http://www.dummies.com/programming/java/use-array-lists-in-java/
+     */
+    ArrayList<Ball> theBalls = new ArrayList<Ball>();
 
 
     /**
@@ -67,110 +65,80 @@ public class TestAnimator implements Animator {
      *
      *
      */
-    public void reverseXSpeed() {
-        // set our instance variable
-        xSpeed = -xSpeed;
-    }
-    public void reverseYSpeed(){
-        ySpeed = -ySpeed;
 
-    }
+    /** External Citation
+     * Date: 3/19/2018
+     * Problem: unsure of how to get the balls to bounce
+     * Resource: https://www.ntu.edu.sg/home/ehchua/programming/java/J8a_GameIntro-BouncingBalls.html
+     * Solution: I used the example code to figure out the x,y wall relationships
+     */
+
 
     /**
      * Action to perform on clock tick
      *
      * @param g the graphics object on which to draw
      */
-    /**
-     * I was unsure of how to get the balls to bounce off of the
-     * walls so i looked on stack overflow for inspiration
-     * source: https://www.ntu.edu.sg/home/ehchua/programming/java/J8a_GameIntro-BouncingBalls.html
-     * date: 3/20/18
-     * */
     public void tick(Canvas g) {
-        // bump our count either up or down by one, depending on whether
-        // we are in "backwards mode".
-//        if (reverseX) {
-//            xcount--;
-//        }
-//        else {
-//            xcount++;
-//        }
-//        if (reverseY) {
-//            ycount--;
-//        }
-//        else {
-//            ycount++;
-//        }
+        //Will iterate over the arrayList of Balls and animate all of them
+        for (Ball b :theBalls) {
+            //moves the ball according to its assigned speed
+            b.numX = b.numX + b.xSpeed;
+            b.numY = b.numY + b.ySpeed;
+            //check if the ball hits the top or bottom of the screen
+            if (b.numY + 60 > BOX_HEIGHT) b.reverseYSpeed();
+            if (b.numY - 60 < 0) b.reverseYSpeed();
+            //check of ball hits right side of screen
+            if (b.numX + 60 > BOX_WIDTH) b.reverseXSpeed();
+            //if the ball hits the bottom right corner
+            if ((b.numY - 60 < 0) && (b.numX + 60 > BOX_WIDTH)) {
+                b.reverseYSpeed();
+                b.reverseXSpeed();
+            }
+            //if the ball hits the top right corner
+            if ((b.numY + 60 > BOX_HEIGHT) && (b.numX + 60 > BOX_WIDTH)) {
+                b.reverseYSpeed();
+                b.reverseXSpeed();
+            }
+            //if the ball hits the paddle
+            if ((b.numX - 60 < 0) && (b.numY <= 1000) && (b.numY >= 600)) b.reverseXSpeed();
 
+            //if the ball hits the left side but not the paddle respawn in a
+            // random location and with random speed
+            if ((b.numX - 60 < 0) && ((b.numY > 1000) || (b.numY < 600))) {
+                //random location
+                b.numX = r.nextInt(2400) + 100;
+                b.numY = r.nextInt(1400) + 100;
+                //random speed
+                //makes a random speed for the ball in (-60,-15)U(15,60)
+                int n = r.nextInt(91) - 45;
+                if (n < 0) {
+                    n = n - 15;
+                } else {
+                    n = n + 15;
+                }
+                b.ySpeed = n;
+                n = r.nextInt(91) - 45;
+                if (n < 0) {
+                    n = n - 15;
+                } else {
+                    n = n + 15;
+                }
+                b.xSpeed = n;
 
-        // Determine the pixel position of our ball.  Multiplying by 15
-        // has the effect of moving 15 pixel per frame.  Modding by 600
-        // (with the appropriate correction if the value was negative)
-        // has the effect of "wrapping around" when we get to either end
-        // (since our canvas size is 600 in each dimension).
-        numX = numX+xSpeed;
-        numY = numY+ySpeed;
-        //check if the ball hits the top or bottom of the screen
-        if (numY+60 > BOX_HEIGHT) this.reverseYSpeed();
-        if(numY-60 < 0) this.reverseYSpeed();
-        //check of ball hits right side of screen
-        if (numX+60 > BOX_WIDTH) this.reverseXSpeed();
-        //if the ball hits the bottom right corner
-        if ((numY-60 < 0)&&(numX+60 > BOX_WIDTH)){
-            this.reverseYSpeed() ;
-            this.reverseXSpeed();
+            }
+
+            // Draw the ball in the correct position.
+            Paint redPaint = new Paint();
+            redPaint.setColor(Color.RED);
+            g.drawCircle(b.numX, b.numY, 60, redPaint);
+            redPaint.setColor(0xff0000ff);
         }
-
-        //if the ball hits the top right corner
-        if ((numY+60 > BOX_HEIGHT)&&(numX+60 > BOX_WIDTH)){
-            this.reverseYSpeed() ;
-            this.reverseXSpeed();
-        }
-        //if the ball hits the paddle
-        if((numX-60 < 0)&&(numY<=1000)&&(numY>=600)) this.reverseXSpeed();
-
-        //if the ball hits the left side but not the paddle
-        if ((numX-60 < 0)&&((numY>1000)||(numY<600))){
-            //Log.i("not paddle", numX+","+numY);
-            this.numX = randomX.nextInt(2400)+100;
-            this.numY = randomX.nextInt(1400)+100;
-            //random speed
-            this.xSpeed=randomX.nextInt(100)-50;
-            int n= randomX.nextInt(91)-45;
-            if (n<0){
-                n=n-15;
-            }
-            else{
-                n=n+15;
-            }
-            this.ySpeed=n;
-            n= randomX.nextInt(91)-45;
-            if (n<0){
-                n=n-15;
-            }
-            else{
-                n=n+15;
-            }
-            this.xSpeed=n;
-
-        }
-
-//ball.xSpeed when you make the array list
-//speed and num instance variables will go to the ball object
-
-
-        // Draw the ball in the correct position.
-        Paint redPaint = new Paint();
-        redPaint.setColor(Color.RED);
-        g.drawCircle(numX, numY, 60, redPaint);
-        redPaint.setColor(0xff0000ff);
-
 
         //Draw a paddle
         // Will eventually be animated
         Paint blackPaint = new Paint();
-        redPaint.setColor(Color.BLACK);
+        blackPaint.setColor(Color.BLACK);
         g.drawRect(0, 600, 10, 1000, blackPaint);
 
     }
@@ -194,18 +162,20 @@ public class TestAnimator implements Animator {
     }
 
     /**
-     * reverse the ball's direction when the screen is tapped
+     * make a new ball when the screen is touched
+     * @param event what the user does to the screen
      */
     public void onTouch(MotionEvent event)
     {
-        /**if (event.getAction() == MotionEvent.ACTION_DOWN)
         {
-            use an arraylist of ball
-         loop through all the balls and do the same thing
-         create a ball object (class) with 4 intance variables speeds and positions
-
+            //coordinates of the finger touch
+         int initialX = (int) event.getX();
+         int initialY = (int) event.getY();
+            //make a new Ball at the place touched
+         Ball ball = new Ball(initialX, initialY);
+            //add the ball to the array list
+         theBalls.add(ball);
         }
-         */
     }
 
 
